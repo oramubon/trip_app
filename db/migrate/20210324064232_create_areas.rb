3a2e1ff947864237037d83d23b6ec756d1f74976
oramubon <cz.toshiki@gmail.com>
2021-03-24 15:49:00 +0900

    areaテーブル作成

---
+++ db/migrate/20210324064232_create_areas.rb
@@ -0,0 +1,11 @@
+class CreateAreas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :areas do |t|
+      t.integer    :country,  null: false
+      t.string     :stste,    null: false
+      t.integer    :language, null: false
+      t.references :user,     foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_063422) do
+ActiveRecord::Schema.define(version: 2021_03_24_064232) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2021_03_24_063422) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "country", null: false
+    t.string "stste", null: false
+    t.integer "language", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_areas_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +61,5 @@ ActiveRecord::Schema.define(version: 2021_03_24_063422) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "areas", "users"
 end
