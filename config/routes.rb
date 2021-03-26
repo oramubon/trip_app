870d13b9910eed5f0b1a9854b9ad475d6ad0494e
oramubon <cz.toshiki@gmail.com>
2021-03-26 21:49:06 +0900

    プロフィール画像登録ページへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get  'areas', to: 'users/registrations#new_area'
     post 'areas', to: 'users/registrations#create_area'
+    get  'images', to: 'users/registrations#new_image'
+    post 'images', to: 'users/registrations#create_image'
+    
   end
   root to: 'tweets#index'
   resources :users, only: :show
