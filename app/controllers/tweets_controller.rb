5d895c77528ce335c053816aeab1cd010a74166c
oramubon <cz.toshiki@gmail.com>
2021-04-02 17:17:27 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index,      only: [:edit, :update, :destroy]
 
   def index
-    @tweets = Tweet.order("created_at DESC")
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
