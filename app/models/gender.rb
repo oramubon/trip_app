c572064bc830d96adb22ab9b0a828555fc19430c
oramubon <cz.toshiki@gmail.com>
2021-04-10 01:53:15 +0900

    ユーザーに関するビューの編集

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,8 +1,8 @@
 class Gender < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'man' },
-    { id: 3, name: 'woman' },
+    { id: 2, name: '男性' },
+    { id: 3, name: '女性' },
   ]
   
   include ActiveHash::Associations
